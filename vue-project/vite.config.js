e39fd5dc51f655d6a69e8efff5c7baa6cc3e9a81
Mike Williams <business.michael.m.williams@gmail.com>
2025-07-14 01:04:06 -0400

    Update vite.config.js

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -2,30 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-          dest: ''
-        }
-      ]
-    })
+    vueDevTools()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'pdfjs-dist': 'pdfjs-dist/legacy/build/pdf.js'
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },
-  optimizeDeps: {
-    include: ['pdfjs-dist']
   },
   build: {
     commonjsOptions: {
